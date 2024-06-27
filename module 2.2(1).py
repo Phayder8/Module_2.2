967d1e1c32985756cf8a41d1e59bde40c3443ada
Phayder8 <witcher2567@gmail.com>
2024-06-27 19:38:43 +0300

    Add files via upload

---
+++ module 2.2(1).py
@@ -0,0 +1,9 @@
+first = 8
+second = 8
+third = 8
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
---
+++ module 2.2(2).py
@@ -0,0 +1,13 @@
+first = 8
+second = 8
+third = 8
+if first == second == third:
+    print(3)
+elif first == second:
+    print(2)
+elif first == third:
+    print(2)
+elif second == third:
+    print(2)
+else:
+    print(0)
